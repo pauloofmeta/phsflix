85f6483fcc33ac409dcd862c462721352a63ef6c
Paulo Henrique Sousa da Silva <pauloofmeta@gmail.com>
2021-03-02 23:22:20 -0300

    Start movies list layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { MovieList } from "./Components/MovieList";
+import { Navbar } from "./Components/Navbar";
+import { GlobalStyle } from "./styles/GlobalStyle";
+import { defaultTheme } from "./styles/theme";
 
 export default function App() {
   return (
-    <div>Hello</div>
+    <ThemeProvider theme={defaultTheme}>
+      <Navbar />
+      <MovieList />
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
---
+++ src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const defaultTheme = {
+  colors: {
+    white: '#E1E1E6',
+    red: '#bd2f33'
+  },
+  backgrounds: {
+    lightest: '#252131',
+    dark: '#191622'
+  }
+}
